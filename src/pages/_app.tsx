b1fa56748339fe33e27b1c3aecfd6ae58eab39e9
NextSap <nextsap@outlook.com>
2023-03-24 15:58:56 +0100

    Fix console warnings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import type {AppProps} from 'next/app';
 import "./base.css";
 import localFont from 'next/font/local'
+import Head from "next/head";
 
 // Font files can be colocated inside of `pages`
-const myFont = localFont({ src: "../../public/Montserrat-Regular.ttf" })
+const myFont = localFont({src: "../../public/Montserrat-Regular.ttf"})
 export default function App({Component, pageProps}: AppProps) {
     return (
-        <main className={myFont.className}>
-            <Component {...pageProps} />
-        </main>
+        <>
+            <Head>
+                <title>AlgoFacade</title>
+            </Head>
+            <main className={myFont.className}>
+                <Component {...pageProps} />
+            </main>
+
+        </>
     );
 }
