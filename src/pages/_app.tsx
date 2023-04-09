7ab442ccf81edb695c0ba2f36200557c8bd60bda
NextSap <nextsap@outlook.com>
2023-04-09 11:19:00 +0200

    compressing images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,8 +9,8 @@ export default function App({Component, pageProps}: AppProps) {
     return (
         <>
             <Head>
-                <title>Algo Façade</title>
-                <link rel="shortcut icon" href="/assets/ShortLogo.ico" />
+                <title>Algo Façade - Nettoyage de façades en Wallonie et à Bruxelles</title>
+                <link rel="shortcut icon" href="/assets/images/other/ShortLogo.ico" />
                 <meta
                     name="description"
                     content="Algo Façade, votre expert en nettoyage de façades, terrasses et toitures. Nos professionnels qualifiés utilisent des méthodes respectueuses de l'environnement pour éliminer les algues, mousses et autres salissures qui ternissent l'aspect de votre habitation. Contactez-nous dès maintenant pour un devis gratuit et retrouvez une maison comme neuve !"
